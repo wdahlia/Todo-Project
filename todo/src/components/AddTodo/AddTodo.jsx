167747391c1c47166c0c9b14ad2ae446bde6a71f
dahlia <wlstnrdahlia@gmail.com>
2023-02-23 18:10:51 +0900

    bug : onAdd props로 로직 받아오는 것에 not a function 오류 발생

--- todo/src/components/AddTodo/AddTodo.jsx
+++ todo/src/components/AddTodo/AddTodo.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import styles from './AddTodo.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 // input text에 todo 추가해서 submit 하는 부분
 // add를 하는 default 값은 active 상태로 들어가게 되는 것
-export default function AddTodo({ add}) {
+// onAdd를 prop 인자로 받아옴 이는 TodoList에서 받아 오는 것
+export default function AddTodo({ onAdd }) {
   const [text, setText] = useState('');
   const handleValue = (e) => {
     // 즉 value값의 변화 인자를 setText 안에 넣어주어야 하므로 로직 작성
@@ -13,6 +15,13 @@ export default function AddTodo({ add}) {
   }
   const handleSubmit = (e) => {
     e.preventDefault();
+    // submit 기본 동작 막아두기
+    console.log(uuidv4(), text);
+    // onAdd({ id: uuidv4(), text, status: 'active'});
+    // uuidv4 사용 2.71 * 10^18 개의 UUID를 생성했을 때 최소 1개가 중복(충돌) 될 확률이 약 50%
+    // 즉 값이 중복될 가능성이 현저히 적음 // 고유 코드로 사용 가능
+    // id값, text, status를 저장해서 상위 컴포넌트에 던져주게 된다
+    setText(''); // setText를 비워준다
   };
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
@@ -21,7 +30,8 @@ export default function AddTodo({ add}) {
       placeholder="할 일을 입력하세요." 
       className={styles.input}
       value={text}
-      onChange={handleValue}/>
+      onChange={handleValue}
+      spellcheck="false"/>
       <button className={styles.btn}></button>
     </form>
   );
--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import AddTodo from '../AddTodo/AddTodo';
 
 
 // ul 부분의 컴포넌트 작성
 // TodoList 컴포넌트 내에 Todo 컴포넌트 배치 
 export default function TodoList() {
+  const handleAdd = (todo) => {
+    // console.log(todo);
+    // input에 submit 된 값을 받아와서 그 인자는 todo, todo리스트를 업데이트 하는 로직 작성
+  }
   return (
-    <div>
-    </div>
+    <AddTodo onAdd={handleAdd} />
   );
 }
 
